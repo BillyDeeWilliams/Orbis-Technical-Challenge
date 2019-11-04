f75d97577cf43258131e9360c40b785614f4da1e
William Madden <madden.william.d@gmail.com>
2019-11-04 13:30:11 -0800

    dispalying symobls and captuing in app state

--- src/components/searchparams.js
+++ src/components/searchparams.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const SearchParamsList= (props) => {
-console.log("search oaram props: " + props)
+
+console.log("search param props: " + props.symbols)
+
+
        return (
            <div className="ui segment">
                <ul className="ui symbolList">
-            
+               { props.symbols.map(item => (
+                       <li key={item}>{item}</li>
+                     ))}
            </ul>
          </div>
        )
