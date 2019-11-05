6e01021a816619cc175175bdbd87dbb98036e310
William Madden <madden.william.d@gmail.com>
2019-11-04 19:40:48 -0800

    debugging

--- src/components/searchparams.js
+++ src/components/searchparams.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 class SearchParamsList extends React.Component  {
 
-    constructor(props) {
-        super(props);
-    }
+
+    state = {
+        symbols : this.props.symbols,
+    };
+
 
   removeSymbol = (i) => {
-      let symbols = props.state.symbols.filter((item, j) => i !== j);
+      let symbols = state.symbols.filter((item, j) => i !== j);
       return {
         symbols,
       };
@@ -19,7 +21,7 @@ class SearchParamsList extends React.Component  {
        return (
            <div className="ui segment">
             <ul className="ui symbolList">
-                { props.state.symbols.map((symbol, index) => (
+                {this.state.symbols.map((symbol, index) => (
                     <li key={index}>{symbol}<button type="button" onClick={() => this.removeSymbol(index)}>X</button></li>
                 ))}
             </ul>
