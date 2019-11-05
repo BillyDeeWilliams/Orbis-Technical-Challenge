740a8d0bf3f03d3173db12ea1e11519dc3e0ba2e
William Madden <madden.william.d@gmail.com>
2019-11-04 19:56:13 -0800

    debugging

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component  {
 onFormSubmit = (e) =>{
     e.preventDefault();
     this.props.onSubmit(this.state.sq)
-    this.state.sq = '';
+    //this.state.sq = '';
 };
 
     render(){
